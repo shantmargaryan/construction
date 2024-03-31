541e4be2e638ac12b144e2f113fd5f161ce8529a
Shant <shant212205@gmail.com>
2024-03-31 07:00:27 -0800

    update

--- js/main.js
+++ js/main.js
@@ -76,3 +76,103 @@ mediaQueryMinWidth_1200.addEventListener("change", (e) => {
    }
    return false;
 });
+
+
+// scroll animation
+
+function getScrollAnimation() {
+   window.addEventListener("scroll", getScrollAnimation);
+   const scrollAnim = document.querySelectorAll(".scroll-anim");
+   if (scrollAnim.length > 0) {
+      scrollAnim.forEach(animItem => {
+         const { top, height } = animItem.getBoundingClientRect();
+         const offset = window.scrollY + top
+         const scrollAnimPoint = window.innerHeight - height / 3
+         if (offset < window.scrollY + scrollAnimPoint && offset + height > window.scrollY) {
+            animItem.classList.add("scroll-anim_active")
+         } else {
+            if (!animItem.classList.contains("scroll-anim_once")) {
+               animItem.classList.remove("scroll-anim_active")
+            }
+         }
+      });
+   }
+};
+
+setTimeout(getScrollAnimation, 700);
+
+// end scroll animation
+
+// start lightbox
+
+const projectItem = document.querySelectorAll(".project__item");
+const lightbox = document.querySelector(".lightbox");
+const lightboxWrapper = document.querySelector(".lightbox__wrapper");
+const lightboxImg = document.querySelector(".lightbox__img");
+const lightboxClose = document.querySelector(".lightbox__close");
+const lightboxNext = document.querySelector(".lightbox__next");
+const lightboxPrev = document.querySelector(".lightbox__prev");
+
+
+let index = 1
+
+
+function showLightBox(lightBox) {
+   if (lightbox > projectItem.length) {
+      index = 1
+   } else if (lightbox < 1) {
+      index = projectItem.length
+   }
+
+   let imgLocation = projectItem[index - 1].children[0].getAttribute("src")
+   lightboxImg.setAttribute("src", imgLocation);
+}
+
+function currentImg() {
+   lightbox.style.opacity = "1";
+   lightbox.style.pointerEvents = "all"
+
+   let imageIndex = parseInt(this.getAttribute("data-item"));
+   showLightBox(index = imageIndex);
+}
+
+for (let i = 0; i < projectItem.length; i++) {
+   projectItem[i].addEventListener("click", currentImg);
+}
+
+function sliderImage(slider) {
+   if (slider < 0) {
+      index = projectItem.length
+   }
+   if (slider > projectItem.length) {
+      index = 1
+   }
+}
+
+function prevImage() {
+   sliderImage(index -= 1);
+   showLightBox(index);
+}
+
+function nextImage() {
+   sliderImage(index += 1);
+   showLightBox(index);
+}
+
+lightboxPrev.addEventListener("click", prevImage);
+lightboxNext.addEventListener("click", nextImage);
+
+
+lightbox.addEventListener("click", (event) => {
+   if (event.target === lightboxWrapper) {
+      lightbox.style.opacity = "0"
+      lightbox.style.pointerEvents = "none";
+   }
+   if (event.target === lightboxClose) {
+      lightbox.style.opacity = "0"
+      lightbox.style.pointerEvents = "none";
+   }
+});
+
+
+// end lightbox
